d0f55d17984f560788b29340d878666e0eeb825c
piechotam <michalpiechotaa13@gmail.com>
2024-07-11 11:16:05 +0200

    api conn, preprocessing

---
+++ src/comments_extraction.py
@@ -0,0 +1,66 @@
+import requests
+import time
+import pandas as pd
+
+
+def extract_video_comments(API_KEY: str, VIDEO_ID: str, order: str, n=100) -> pd.DataFrame:
+    """
+    Returns a DataFrame of n (default 100) comments from a video 
+    with the provided VIDEO_ID and API_KEY.
+    
+    The order parameter specifies the order of comments. Valid values are: 'time', 'relevance'.
+    
+    Parameters:
+        API_KEY (str): YouTube Data API v3 key.
+        VIDEO_ID (str): The ID of the YouTube video.
+        order (str): Order of comments, either 'time' or 'relevance'.
+        n (int, optional): Number of comments to retrieve. Default is 100.
+    
+    Returns:
+        pd.DataFrame: A DataFrame containing the comments with their publish date and text.
+    """
+    if order not in ['time', 'relevance']:
+        raise ValueError("Invalid value for order parameter. Valid values are: 'time', 'relevance'.")
+
+    url = 'https://www.googleapis.com/youtube/v3/commentThreads'
+    params = {
+        'key': API_KEY,
+        'videoId': VIDEO_ID,
+        'part': 'snippet',
+        'maxResults': 100,  # API max results per page
+        'textFormat': 'plainText',
+        'order': order
+    }
+
+    comments = []
+    total_fetched = 0
+
+    while total_fetched < n:
+        response = requests.get(url, params=params)
+        if response.status_code != 200:
+            response.raise_for_status()
+        data = response.json()
+        time.sleep(1)
+        
+        for item in data.get('items', []):
+            item_details = item['snippet']['topLevelComment']['snippet']
+            comments.append({'date': item_details['publishedAt'], 'text': item_details['textDisplay']})
+            total_fetched += 1
+            if total_fetched >= n:
+                break
+
+        if 'nextPageToken' not in data:
+            break
+
+        params['pageToken'] = data['nextPageToken']
+    
+    df = pd.DataFrame(comments)
+    return df
+
+if __name__ == '__main__':
+    # testing
+    API_KEY = 'AIzaSyCU5HvKL7_pZtcT8oCsj2N56gifJJfCiQo'
+    VIDEO_ID = 'fklHBWow8vE'
+
+    df = extract_video_comments(API_KEY, VIDEO_ID, order='relevance')
+    print(df.head())
---
+++ src/processing.py
@@ -0,0 +1,194 @@
+import re
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+def read_and_split(path: str) -> tuple:
+    """
+    Read the data from a CSV file, split it into training and testing sets, and return the split datasets.
+
+    Args:
+        path (str): The path to the CSV file.
+
+    Returns:
+        tuple: A tuple containing the training and testing datasets in the following order:
+            - X_train: The training features.
+            - y_train: The training labels.
+            - X_test: The testing features.
+            - y_test: The testing labels.
+    """
+    try:
+        df = pd.read_csv(path, encoding='ISO-8859-1', usecols=[0, 2, 5], header=0, names=['target', 'date', 'text'])
+    except Exception as e:
+        raise ValueError(f"Error reading the CSV file: {e}")
+
+    df_train, df_test = train_test_split(df, test_size=0.2, random_state=21)
+
+    y_train, y_test = df_train['target'], df_test['target']
+    X_train, X_test = df_train.drop(columns='target'), df_test.drop(columns='target')
+
+    return X_train, y_train, X_test, y_test
+
+def replace_emojis(text: str) -> str:
+    """
+    Replace emojis with keywords EMO_POS and EMO_NEG for positive and negative emojis, respectively.
+
+    Args:
+        text (str): The text to be processed.
+
+    Returns:
+        str: The processed text.
+    """
+    # Smile -- :), : ), :-), (:, ( :, (-:
+    text = re.sub(r'(:\s?\)|:-\)|\(\s?:|\(-:|:\'\))', ' EMO_POS ', text)
+    # Laugh -- :D, : D, :-D, xD, x-D, XD, X-D
+    text = re.sub(r'(:\s?D|:-D|x-?D|X-?D)', ' EMO_POS ', text)
+    # Love -- <3, :*
+    text = re.sub(r'(<3|:\*)', ' EMO_POS ', text)
+    # Wink -- ;-), ;), ;-D, ;D, (;,  (-;
+    text = re.sub(r'(;-?\)|;-?D|\(-?;)', ' EMO_POS ', text)
+    # Sad -- :-(, : (, :(, ):, )-:
+    text = re.sub(r'(:\s?\(|:-\(|\)\s?:|\)-:)', ' EMO_NEG ', text)
+    # Cry -- :,(, :'(, :"(
+    text = re.sub(r'(:,\(|:\'\(|:"\()', ' EMO_NEG ', text)
+    
+    return text
+
+def clean_text(text: str) -> str:
+    """
+    Process the given text by transforming it to lowercase, replacing emojis, removing mentions and URLs, and replacing multiple spaces with a single space.
+
+    Args:
+        text (str): The text to be processed.
+
+    Returns:
+        str: The processed text.
+    """
+    # transform to lowercase
+    text = text.lower()
+    # replace emojis
+    text = replace_emojis(text)
+    # remove mentions
+    text = re.sub(r'@[\S]+', '', text)
+    # remove hashtags
+    text = re.sub(r'#[\S]+', '', text)
+    # remove urls
+    text = re.sub(r'((www\.[\S]+)|(https?://[\S]+))', '', text)
+    # replace multiple spaces with single space
+    text = re.sub(r'\s+', ' ', text)
+
+    return text
+
+def tokenize(text: str) -> list:
+    """
+    Tokenizes the given text into a list of words.
+    
+    Args:
+        text (str): The input text to be tokenized.
+        
+    Returns:
+        list: A list of words extracted from the input text.
+    """
+    words = word_tokenize(text)
+    
+    return words
+
+import re
+
+def is_word_correct(word: str) -> bool:
+    """
+    Check if a word consists of only alphabetic characters.
+
+    Args:
+        word (str): The word to be checked.
+
+    Returns:
+        bool: True if the word consists of only alphabetic characters, False otherwise.
+    """
+    pattern = r'^[A-Za-z]+$'
+    return re.search(pattern, word) is not None
+
+def extract_correct_words(words: list) -> list:
+    """
+    Extracts the correct words from a list of words.
+    
+    Args:
+        words (list): A list of words.
+        
+    Returns:
+        list: A list of correct words.
+    """
+    correct_words = [word for word in words if is_word_correct(word)]
+
+    return correct_words
+
+def remove_stop_words(words: list) -> list:
+    """
+    Removes stop words from a list of words.
+
+    Args:
+        words (list): A list of words.
+
+    Returns:
+        list: A list of words with stop words removed.
+    """
+    stop_words = set(stopwords.words('english'))
+    filtered_words = [word for word in words if not word in stop_words]
+
+    return filtered_words
+
+def stem_words(words: list) -> list:
+    """
+    Stem the given list of words using Porter Stemmer algorithm.
+    
+    Args:
+        words (list): A list of words to be stemmed.
+        
+    Returns:
+        list: A list of stemmed words.
+    """
+    porter_stemmer = PorterStemmer()
+    stemmed_words = [porter_stemmer.stem(word) for word in words]
+ 
+    return stemmed_words
+
+def process_tweet(tweet: str) -> list:
+    """
+    Process a tweet by cleaning, tokenizing, removing stop words, and stemming the words.
+
+    Args:
+        tweet (str): The input tweet to be processed.
+
+    Returns:
+        list: A list of stemmed words after processing the tweet.
+    """
+    cleaned_tweet = clean_text(tweet)
+    words = tokenize(cleaned_tweet)
+    words_filtered = remove_stop_words(words)
+    words_stemmed = stem_words(words_filtered)
+
+    return words_stemmed
+
+def create_dictionary(tweets: pd.Series) -> dict:
+    """
+    Creates a dictionary of words and their frequencies from a series of tweets.
+
+    Args:
+        tweets (pd.Series): A series of tweets.
+
+    Returns:
+        dict: A dictionary where the keys are words and the values are their frequencies.
+    """
+    words_dict = dict()
+
+    for tweet in tweets:
+        words = process_tweet(tweet)
+        for word in words:
+            if word not in words_dict:
+                words_dict[word] = 1
+            else:
+                words_dict[word] += 1
+    
+    return words_dict
